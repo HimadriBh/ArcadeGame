2ce6b7628ba0158d912ccab6aa30b6004e8a0ae5
Himadri Bhargava <himadribhargava@gmail.com>
2019-06-17 13:00:23 +0530

    checkCollion method implemented

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.posx = 0;
-    this.posy = assignPositions();
+    this.posx = -20;
+    this.posy = enemyoffset();
+    this.speed =  Math.random()*300 + 200;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -16,10 +17,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     if(this.posx > 500){
-        this.posx = 0;
-        this.posy = assignPositions();
+        this.posx = -20;
+        this.posy = enemyoffset();
     }
-    this.posx += assignPositions()*dt;
+    this.posx += this.speed*dt;
 };
 
 
@@ -70,7 +71,7 @@ document.addEventListener('keyup', function(e) {
 });
 
 
-function assignPositions(){
+function enemyoffset(){
     var posValues = [60, 140, 220];
     var posIdx =  Math.floor(Math.random()*posValues.length);
     return posValues[posIdx];
@@ -100,5 +101,28 @@ player.handleInput = function(inputKey){
         if(this.y < 380){
             this.y += moveDown;
         }
+    }
+    checkWin();
+}
+
+function checkCollisions(){
+    allEnemies.forEach((enemy)=>{
+        var proximity = getDistance(enemy.posx, enemy.posy, player.x, player.y)
+        if(proximity < 60){
+            player.x = 0;
+            player.y = 380;
+        }
+    })
+}
+
+function getDistance(x1, y1, x2, y2){
+    let disx = x2 -x1;
+    let disy = y2 -y1;
+    return Math.sqrt(Math.pow(disx, 2) + Math.pow(disy, 2));
+
+}
+
+function checkWin(){
+    if(player.y === -20){
     }
 }
